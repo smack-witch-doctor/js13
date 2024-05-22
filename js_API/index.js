dd9ae56534657caf30e8ab3269d424181210d26d
smack-witch-doctor <160270838+smack-witch-doctor@users.noreply.github.com>
2024-05-22 19:20:25 +0200

    Add files via upload

---
+++ js_API/index.js
@@ -0,0 +1,41 @@
+
+//minifikacija, nmp instaal !test
+
+//ovo je zadatak za test:!
+//JS:!
+//funkcija za je li broj unesen ukoliko su oba unosa broj, vrati onaj veci, a ako su jednaki vrati prvi
+//u label ispisati gresku ako nije broj
+//css:
+//ukoliko funkcija vrati ispravnu vrijednost, obojajaj pozadinu treceg textboxa
+//u zelenu
+//ukoliko je funkcija vratila gresku obojati pozadinu treceg textboxa u crvenu boju
+//HTML:
+//dodaj gumb koji poziva funkciju onclick i dodaj paragraf za prikaz rezultata
+
+
+
+function changeColor(element) {
+    var currentColor = element.style.backgroundColor;
+    if (currentColor == "red") {
+        element.style.backgroundColor = "green";
+    }
+    
+    else {
+        element.style.backgroundColor = "red";
+    }
+}
+function klikButton(){
+var prviUnos= getElementById(unos1);
+var drugiUnos= getElementById(unos2);
+
+if (typeof prviUnos=="number"){
+  
+
+
+}
+}
+
+
+/*let input3=document.getElementById("mojButton");
+    
+input3.style.backgroundColor="white";*/
